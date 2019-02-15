094fc9b83acfdf72379e2f8ca01ae1ecde513370
lijiancheng <1833717745@qq.com>
2019-02-15 19:13:14 +0800

    // 更新小程序 appid 和秘钥

--- src/common/config/config.js
+++ src/common/config/config.js
@@ -2,8 +2,8 @@
 module.exports = {
   default_module: 'api',
   weixin: {
-    appid: '', // 小程序 appid
-    secret: '', // 小程序密钥
+    appid: 'wx07dad7ac86ee88f8', // 小程序 appid
+    secret: '55da5261e15827602f97646f686a4403', // 小程序密钥
     mch_id: '', // 商户帐号ID
     partner_key: '', // 微信支付密钥
     notify_url: '' // 微信异步通知，例：https://www.nideshop.com/api/pay/notify
