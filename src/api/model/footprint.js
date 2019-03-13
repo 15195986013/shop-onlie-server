0eafd1aa28123c010059b6fd57d81af1487c60f0
lijiancheng <1833717745@qq.com>
2019-03-13 23:23:46 +0800

    // 改一下time stamp 的字段 // 改一下uuid

--- src/api/model/footprint.js
+++ src/api/model/footprint.js
@@ -1,12 +1,14 @@
 module.exports = class extends think.Model {
   async addFootprint(userId, goodsId) {
+    const that = this;
     // 用户已经登录才可以添加到足迹
     // 时间需要修改
     if (userId > 0 && goodsId > 0) {
       await this.add({
+        id: think.uuid("v4").replace(/-/g,''),
         goods_id: goodsId,
         user_id: userId,
-        add_time: parseInt(Date.now() / 1000)
+        add_time: ['exp', 'CURRENT_TIMESTAMP()']
       });
     }
   }
